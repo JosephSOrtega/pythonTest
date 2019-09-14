d328cdd35ee1a812f4d3e4a5b9e8e71b3a27b897
JustinSOrtega <justinsortega@gmail.com>
2019-09-14 11:17:43 -0500

    working on func now

---
+++ resources/META-INF/Testing.py
@@ -0,0 +1,52 @@
+print("Hi, Everybody!") # string
+print('Good morning')
+print("It's a beautiful day in the neighborhood")
+print("Today is Saturday and we're doing Python")
+print(5)
+
+message = "dogs are dope"
+print(message)
+
+
+num = 1
+number = 1
+
+print(num + number)
+
+
+favNum = 42
+
+
+
+message = "my fav num is " + str(favNum)
+print(message)
+
+
+# Conditional execution
+doing_python_right_now = True
+on_mars_right_now = False
+is_raining = False
+apples = "delicious"
+arr = [1,3,5,2,7]
+
+
+if(doing_python_right_now == True):
+    print("Yay Python! Practice makes permanent")
+if(on_mars_right_now):
+    print("How long do you plan to stay on Mars?")
+else:
+    print("I'm glad we're on Spaceship Earth.")
+
+
+
+if(is_raining == True):
+    print("Bring an umbrella")
+
+if(apples == "delicious"):
+        print ("of course")
+else:
+        print ("YOU LIE")
+print ("ended statement")
+print(arr)
+arr.sort()
+print(arr)
