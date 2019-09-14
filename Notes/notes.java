3b6401563e458c4cd7e92f9f17f245e589ccf3d9
JustinSOrtega <justinsortega@gmail.com>
2019-09-14 10:33:13 -0500

    ITS ALIVE

---
+++ Notes/notes.java
@@ -0,0 +1,25 @@
+public  class notes{
+
+    public static void main(String[] args) {
+
+        System.out.println("Types of machine learning\n\n" +
+                "- Supervised\n" +
+                "   - Means we have labled data\n" +
+                "       - Human interaction that gives answers\n" +
+                "       - Spam filtering\n" +
+                "       - Pick a picture of a stoplight\n\n" +
+                "- Unsupervised;\n" +
+                "   - No labels on game data\n" +
+                "   - Chess game or go\n" +
+                "   - NLP and clusterting\n" +
+                "   - Videogame opponents\n\n" +
+                "- Semi-supervised\n" +
+                "   - Unsupervised as much as possible\n" +
+                "   - Human interaction when needed\n\n" +
+                "- Reinforcement Learning\n" +
+                "   - 2 algorythm setup\n" +
+                "   - Algorithm A is setup to do a job\n" +
+                "   - Algorithm B is setup as a critic to tell algorithm Bif its doing a good/bad job");
+    }
+
+}
---
+++ resources/META-INF/Notes.py
@@ -0,0 +1,27 @@
+print("Hi, Everybody!") # string
+print('Good morning')
+print("It's a beautiful day in the neighborhood")
+print("Today is Saturday and we're doing Python")
+print(5)
+
+message = "dogs are dope"
+print(message)
+
+print("Types of machine learning\n\n" +
+                   "- Supervised\n" +
+                   "   - Means we have labled data\n" +
+                   "       - Human interaction that gives answers\n" +
+                   "       - Spam filtering\n" +
+                   "       - Pick a picture of a stoplight\n\n" +
+                   "- Unsupervised;\n" +
+                   "   - No labels on game data\n" +
+                   "   - Chess game or go\n" +
+                   "   - NLP and clusterting\n" +
+                   "   - Videogame opponents\n\n" +
+                   "- Semi-supervised\n" +
+                   "   - Unsupervised as much as possible\n" +
+                   "   - Human interaction when needed\n\n" +
+                   "- Reinforcement Learning\n" +
+                   "   - 2 algorythm setup\n" +
+                   "   - Algorithm A is setup to do a job\n" +
+                   "   - Algorithm B is setup as a critic to tell algorithm Bif its doing a good/bad job");
